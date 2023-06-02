ac8d561e2473ba0f664702293139df75dc1cd6b4
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-06-03 01:22:27 +0300

    фреймворк сообщений

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
@@ -60,6 +61,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Профиль успешно обновлен')
+        else:
+            messages.error(request, 'Ошибка при обновлении вашего профиля')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user)
